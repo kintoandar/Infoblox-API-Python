6e206da50d8ee62a5c979be22a7b308b04ed8268
Joel Bastos <kintoandar@gmail.com>
2015-11-17 16:02:28 +0000

    Rearrange project to avoid conflicts on Python Repository

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open('README.txt').read()
 
 setup(
-    name='infoblox',
-    version='1.1',
+    name='pyinfobloxapi',
+    version='1.1.0',
     description='The module implements Infoblox IPAM API via REST API',
     long_description=README,
     license = 'Licensed under the Apache License, Version 2.0',
-    packages=['.'],
+    packages=['pyinfobloxapi'],
     install_requires=['requests'],
+    include_package_data = True,
 )
